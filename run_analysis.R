be168dca10268d52594755a40a91109413e36a58
govinddata <30706303+govinddata@users.noreply.github.com>
2017-08-11 13:20:41 -0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -5,8 +5,8 @@
 ## 2017-08-11
 
 # run_analysis.r File Description:
-# https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
 # This script will perform the following steps on the UCI HAR Dataset downloaded from 
+# https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
 # 1. Merge the training and the test sets to create one data set.
 # 2. Extract only the measurements on the mean and standard deviation for each measurement. 
 # 3. Use descriptive activity names to name the activities in the data set
